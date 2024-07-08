152a3336ed222ddbb5a7f57f3d971e1e90a3b0d5
klmeir <caalmeir@gmail.com>
2024-07-08 17:22:54 -0500

    add operation add room

---
+++ Booking.Api/ApiHandlers/Hotels/HotelAddValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Booking.Application.Hotels;
+
+namespace Booking.Api.ApiHandlers.Hotels
+{
+    public class HotelAddValidator : AbstractValidator<HotelAddCommand>
+    {
+
+        public HotelAddValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.Address).NotEmpty();
+            RuleFor(x => x.Commission).NotEmpty();
+        }
+    }
+}
---
+++ Booking.Api/ApiHandlers/Rooms/RoomApi.cs
@@ -0,0 +1,21 @@
+﻿using Booking.Api.Filters;
+using Booking.Application.Rooms;
+using Booking.Domain.Dtos;
+using MediatR;
+
+namespace Booking.Api.ApiHandlers.Rooms
+{
+    public static class RoomApi
+    {
+        public static RouteGroupBuilder MapRooms(this IEndpointRouteBuilder routeHandler)
+        {
+            routeHandler.MapPost("/", async (IMediator mediator, [Validate] RoomAddCommand room) =>
+            {
+                return Results.Ok(await mediator.Send(room));
+            })
+            .Produces(StatusCodes.Status200OK, typeof(RoomDto));
+
+            return (RouteGroupBuilder)routeHandler;
+        }
+    }
+}
