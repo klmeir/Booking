79b372d55adfe724eccefee0cd88189d53efe66b
klmeir <caalmeir@gmail.com>
2024-07-08 15:52:00 -0500

    Added generic repository, unitofwork, hotel repository, etc.

--- Booking.Infrastructure/Adapters/GenericRepository.cs
+++ Booking.Infrastructure/Adapters/GenericRepository.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Booking.Domain.Entities;
+﻿using Booking.Domain.Entities;
 using Booking.Infrastructure.DataSource;
 using Booking.Infrastructure.Ports;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Booking.Infrastructure.Adapters
 {
@@ -16,12 +17,58 @@ namespace Booking.Infrastructure.Adapters
             _dataset = Context.Set<T>();
         }
 
+
+        public async Task<IEnumerable<T>> GetManyAsync(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string includeStringProperties = "", bool isTracking = false)
+        {
+            IQueryable<T> query = Context.Set<T>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeStringProperties))
+            {
+                foreach (var includeProperty in includeStringProperties.Split
+                    (new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync().ConfigureAwait(false);
+            }
+
+            return (!isTracking) ? await query.AsNoTracking().ToListAsync() : await query.ToListAsync();
+        }
+
         public async Task<T> AddAsync(T entity)
         {
             _ = entity ?? throw new ArgumentNullException(nameof(entity), "Entity can not be null");
             await _dataset.AddAsync(entity);
-            await Context.SaveChangesAsync();
             return entity;
+        }
+
+        public void DeleteAsync(T entity)
+        {
+            _ = entity ?? throw new ArgumentNullException(nameof(entity), "Entity can not be null");
+            _dataset.Remove(entity);
+        }
+
+        public async Task<T> GetOneAsync(int id)
+        {
+            return await _dataset.FindAsync(id) ?? throw new ArgumentNullException(nameof(id));
+
+        }
+
+        public void UpdateAsync(T entity)
+        {
+            _dataset.Update(entity);
         }
 
     }
---
+++ Booking.Infrastructure/Adapters/HotelRepository.cs
@@ -0,0 +1,21 @@
+﻿using Booking.Domain.Entities;
+using Booking.Domain.Ports;
+using Booking.Infrastructure.Ports;
+
+namespace Booking.Infrastructure.Adapters
+{
+    [Repository]
+    public class HotelRepository : IHotelRepository
+    {
+        readonly IRepository<Hotel> _dataSource;
+
+        public HotelRepository(IRepository<Hotel> dataSource) => _dataSource = dataSource
+            ?? throw new ArgumentNullException(nameof(dataSource));
+
+        public async Task<Hotel> SaveHotel(Hotel h) => await _dataSource.AddAsync(h);
+
+        public async Task<Hotel> SingleHotel(int id) => await _dataSource.GetOneAsync(id);
+
+
+    }
+}
