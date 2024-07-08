b5edb260c6c02918dc725c8f0c5e81484653950f
klmeir <caalmeir@gmail.com>
2024-07-08 15:49:38 -0500

    Persistence configuration classes for entities are added.

---
+++ Booking.Infrastructure/DataSource/ModelConfig/GuestConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Booking.Domain.Entities;
+
+namespace Booking.Infrastructure.DataSource.ModelConfig
+{
+    public class GuestConfig : IEntityTypeConfiguration<Guest>
+    {        
+        public void Configure(EntityTypeBuilder<Guest> builder)
+        {
+            builder.Property(b => b.Id).IsRequired();
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.ReservationId).IsRequired();
+            builder.HasIndex(b => b.ReservationId);
+
+            builder.Property(b => b.Name).IsRequired();
+            builder.Property(b => b.Birthdate).IsRequired();
+            builder.Property(b => b.Gender).IsRequired();
+            builder.Property(b => b.DocumentType).IsRequired();
+            builder.Property(b => b.DocumentNumber).IsRequired();
+            builder.Property(b => b.Email).IsRequired();
+            builder.Property(b => b.Phone).IsRequired();
+        }
+    }
+}
